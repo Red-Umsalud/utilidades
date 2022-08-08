f04d6e7822a02ff683585b156e7e06133a78507a
Galo Pocoma <gpocoma@msn.com>
2022-08-08 15:41:27 -0400

    modularizacion basica del generador de qr

--- generaqr.py
+++ generaqr.py
@@ -7,6 +7,7 @@ archivo = open(RUTA_CSV)
 csv_reader = reader(archivo)
 contador = 0
 for i in csv_reader:
-    img = qrcode.make(str(i))
+    contenido = i
+    img = qrcode.make(contenido)
     img.save(f'{contador}-{i[1]}.png')
     contador += 1
