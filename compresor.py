311dd4aee9a90d9443b9f2e609c26498fb061c0c
Galo Pocoma <gpocoma@msn.com>
2022-08-11 14:41:41 -0400

    agregadas nuevas salidas de imagenes

--- compresor.py
+++ compresor.py
@@ -7,12 +7,14 @@ import sys
 
 RUTA_IMAGEN = str(sys.argv[1])
 SIZE_IMAGEN_PEQUE = (450,350)
+SIZE_IMAGEN_CARROUSEL = (1200,700)
+SIZE_IMAGEN_NOTIC = (1200,800)
 SIZE_IMAGEN_BANNER = (1200,400)
-
 
 
 img = Image.open(f'{RUTA_IMAGEN}')
 width, height = img.size
+
 
 output1 = img.resize(SIZE_IMAGEN_PEQUE, Image.ANTIALIAS)
 output1.save('imagen_peque.jpg', quality=50)
@@ -20,4 +22,8 @@ output1.save('imagen_peque.jpg', quality=50)
 output2 = img.resize(SIZE_IMAGEN_BANNER, Image.ANTIALIAS)
 output2.save('imagen_banner.jpg', quality=95)
 
+output3 = img.resize(SIZE_IMAGEN_NOTIC, Image.ANTIALIAS)
+output3.save('imagen_noticia.jpg', quality=95)
 
+output4 = img.resize(SIZE_IMAGEN_CARROUSEL, Image.ANTIALIAS)
+output4.save('imagen_carrousel.jpg', quality=95)
